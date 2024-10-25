e9483ca6d02268063493f865810712f9c1a15a5b
User <hsala089@uottawa.ca>
2024-10-25 10:00:44 -0400

    SCRUM-26: CREATED NEW COMPONENT

---
+++ HmsPmsFrontend/src/app/consult-patient-file/patient-file/location-history/location-history.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-location-history',
+  standalone: true,
+  imports: [],
+  templateUrl: './location-history.component.html',
+  styleUrl: './location-history.component.css'
+})
+export class LocationHistoryComponent {
+
+}
